435952dc619cb9d3f1e84bc818ab2266e1a4d3d8
Rishabh Verma <41505916+rishabhverm123@users.noreply.github.com>
2024-02-21 12:31:24 +0530

    frontend work

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CenterComponent } from './components/center/center.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CenterComponent
   ],
   imports: [
     BrowserModule,
