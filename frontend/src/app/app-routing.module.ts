a6271f268cadf373472eb69943f62ca09a30e268
Rishabh Verma <41505916+rishabhverm123@users.noreply.github.com>
2024-02-26 12:33:42 +0530

    fixing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,22 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path:'',
-  //   pathMatch:'full',
-  //   redirectTo:'main'
-  // },
 
+  {
+    path:'', pathMatch:'full',redirectTo:'main'
+  },
   {
     path:'main',
     loadChildren:()=>
       import('./components/main/main.module').then((m)=>m.MainModule)
   },
-  //   {
-  //   path:'**',
-  //   pathMatch:'full',
-  //   redirectTo:'main'
-  // },
+    {
+    path:'**',
+    redirectTo:'main',
+    pathMatch:'full'
+  },
 
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,7 +16,12 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
