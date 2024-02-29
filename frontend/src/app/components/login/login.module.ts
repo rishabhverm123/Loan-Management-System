441a4f76f7d721cd5417c4c7dd06c10f3d8f7bb8
Rishabh Verma <41505916+rishabhverm123@users.noreply.github.com>
2024-02-29 15:43:09 +0530

    add prime ng

--- frontend/src/app/components/login/login.module.ts
+++ frontend/src/app/components/login/login.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DividerModule
   ]
 })
 export class LoginModule { }
